0b7e4c92c1934d181d9a9d17d97fe1fd3f288c50
Bryson Kern <kernbryson@yahoo.com>
2022-05-18 15:01:41 -0400

    began creating promp

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
-const mysql = require("mysql2")
-const inquirer = require('inquirer')
-const cTable = require('console.table')
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+
+startPrompt();
+
+function startPrompt() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "What would you like to do?",
+        choices: [
+          "view all departments",
+          "view all roles",
+          "view all employees",
+          "add a department",
+          "add a role",
+          "add an employee",
+          "update an employee role",
+        ],
+      },
+    ])
+    .then((answers) => {
+      switch (answers.choice) {
+        case "view all departments":
+          departmentView();
+          break;
+        case "Intern":
+          addIntern();
+          break;
+        default:
+          buildTeam();
+      }
+    });
+}
+function departmentView(){
+    
+}
