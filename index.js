7fc55ac02cc62a9f08f5793de0ee4a0c4caba289
Bryson Kern <kernbryson@yahoo.com>
2022-05-19 14:05:33 -0400

    added add a role and add a job function

--- index.js
+++ index.js
@@ -44,6 +44,9 @@ function startPrompt() {
         case "add a department":
           addDepartment();
           break;
+        case "add a role":
+          addRole();
+          break;
         case "exit":
           exit();
           break;
@@ -79,7 +82,7 @@ function RoleView() {
   });
 }
 function employeeView() {
-  const query = `SELECT role.title AS TITLE, role.salary AS SALARY, role.id AS ROLE_ID FROM role;`;
+  const query = `SELECT employees.first_name AS FIRST_NAME, employees.last_name AS LAST_NAME, employees.id AS EMPLOYEE_ID, employees.manager_id AS MANAGER_ID FROM employees;`;
   connection.query(query, (err, res) => {
     if (err) throw err;
     console.log("\n");
@@ -114,24 +117,30 @@ function addDepartment() {
 }
 
 function addRole() {
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "newDepartment",
-          message: "What department would you like to add?",
-        },
-      ])
-      .then((data) => {
-        let newDept = data.newDepartment;
-        const query = `INSERT INTO department (name) VALUES ('${newDept}');`;
-        connection.query(query, (err, res) => {
-          if (err) throw err;
-          console.log(
-            "\x1b[32m",
-            "Successfully added " + newDept + " department!"
-          );
-          startPrompt();
-        });
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "newTitle",
+        message: "What is the roles title?",
+      },
+      {
+        type: "input",
+        name: "newSalary",
+        message: "What is the roles salary?",
+      },
+    ])
+    .then((data) => {
+      let newTitle = data.newTitle;
+      let newSalary = data.newSalary;
+      const query = `INSERT INTO role (title, salary) VALUES ('${newTitle}',${newSalary});`;
+      connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.log(
+          "\x1b[32m",
+          "Successfully added " + newTitle + " to roles!"
+        );
+        startPrompt();
       });
-  }
+    });
+}
